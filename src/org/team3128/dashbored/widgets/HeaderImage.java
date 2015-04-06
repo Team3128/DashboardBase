4ec217907d243e324f12e4f591fc93fc87e59392
Jamie Smith <jsmith@crackofdawn.onmicrosoft.com>
2015-04-05 22:25:34 -0700

    Initial commit. They tried to stop me by withholding documentation, but I did it anyway! Muahahahahaha!!!

---
+++ src/org/team3128/dashbored/widgets/HeaderImage.java
@@ -0,0 +1,34 @@
+package org.team3128.dashbored.widgets;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+import edu.wpi.first.smartdashboard.gui.StaticWidget;
+import edu.wpi.first.smartdashboard.properties.Property;
+
+public class HeaderImage extends StaticWidget
+{
+	ImageIcon headerIcon;
+
+	JLabel headerLabel;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5524897683464348374L;
+
+	@Override
+	public void propertyChanged(Property arg0)
+	{
+		
+	}
+
+	@Override
+	public void init()
+	{
+		headerIcon = new ImageIcon(getClass().getClassLoader().getResource("org/team3128/dashbored/images/header.png"));
+		headerLabel = new JLabel(headerIcon);
+		
+		add(headerLabel);
+	}
+
+}
